4cf27574a92364850c361fef85346df1a263b832
Avery <averykhoo@gmail.com>
2022-09-29 17:38:30 +0800

    cleanup the fake jwt thing

--- diode_bridge/dependencies/jose_wrapper.py
+++ diode_bridge/dependencies/jose_wrapper.py
@@ -3,19 +3,44 @@ acts as a wrapper for some jwt/jwe library
 undecided which library to use, but it shouldn't matter
 for now, just passthrough
 """
-import json
-from typing import Any
-from typing import Dict
 from typing import Optional
 from typing import Union
 
+from diode_bridge.schemas.layer_1_packet import Packet
 
-def serialize(json_data: Dict[str, Any]) -> bytes:
+
+def serialize(packet: Packet) -> bytes:
     # todo: actually use some jwt and jwe
     # todo: accept symmetric secrets or public/private keys loaded from somewhere
-    return json.dumps(json_data).encode('ascii')
+    return json.dumps({'pretend-this-is-a-jwt-jwe': json.loads(packet.json())}).encode('ascii')
 
 
-def deserialize(binary_data: Union[bytes, bytearray]) -> Optional[Dict[str, Any]]:
+def deserialize(binary_data: Union[bytes, bytearray]) -> Optional[Packet]:
     # todo: reverse of serialize
-    return json.loads(binary_data.decode('ascii'))
+    return Packet.parse_obj(json.loads(binary_data.decode('ascii'))['pretend-this-is-a-jwt-jwe'])
+
+
+if __name__ == '__main__':
+    import datetime
+    import json
+    import random
+    import uuid
+
+    from diode_bridge.schemas.layer_1_packet import Control
+    from diode_bridge.schemas.layer_1_packet import Metadata
+
+    p = Packet(metadata=Metadata(sender_uuid=uuid.uuid4(),
+                                 recipient_uuid=uuid.uuid4(),
+                                 sent_timestamp=datetime.datetime.now(),
+                                 protocol_version=random.randint(0, 99999)),
+               messages=[],
+               control=Control(sender_clock_sender=random.randint(0, 99999),
+                               sender_clock_recipient=random.randint(0, 99999),
+                               sender_clock_out_of_order=[random.randint(0, 9999) for _ in
+                                                          range(random.randint(0, 999))],
+                               recipient_clock_sender=random.randint(0, 99999),
+                               nack_hashes=[str(random.randint(0, 99999)) for _ in range(random.randint(0, 99))]))
+    print(p)
+    print(serialize(p))
+    print(deserialize(serialize(p)))
+    print(deserialize(serialize(p)) == p)
