5a8bcf5262602bcf358ec488fa91c3fa17e5fc8c
Avery Khoo <averykhoo@gmail.com>
2022-09-30 12:03:30 +0800

    add constrained int type

--- diode_bridge/schemas/layer_1_packet.py
+++ diode_bridge/schemas/layer_1_packet.py
@@ -5,6 +5,7 @@ from typing import Optional
 from uuid import UUID
 
 from pydantic import BaseModel
+from pydantic import conint
 
 
 class ContentType(Enum):
@@ -16,20 +17,20 @@ class Metadata(BaseModel):
     sender_uuid: UUID
     recipient_uuid: UUID
     sent_timestamp: datetime.datetime  # todo: this has to be unique per-packet
-    protocol_version: int = 1
+    protocol_version: conint(ge=1) = 1
 
 
 class Message(BaseModel):
-    message_id: int  # more than zero
+    message_id: conint(ge=1)
     content_type: ContentType
     content: bytes
 
 
 class Control(BaseModel):
-    sender_clock_sender: int
-    sender_clock_recipient: int
-    sender_clock_out_of_order: List[int]
-    recipient_clock_sender: int
+    sender_clock_sender: conint(ge=0)
+    sender_clock_recipient: conint(ge=0)
+    sender_clock_out_of_order: List[conint(ge=1)]
+    recipient_clock_sender: conint(ge=0)
     nack_hashes: List[str]
 
 
