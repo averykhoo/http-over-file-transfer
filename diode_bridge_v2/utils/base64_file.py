2a80fd86cdaca416be27d01950c0b6abd65c3cdc
Avery <averykhoo@gmail.com>
2022-10-07 17:51:20 +0800

    refactor to someday support base85

--- diode_bridge_v2/utils/base64_file.py
+++ diode_bridge_v2/utils/base64_file.py
@@ -6,13 +6,13 @@ from typing import BinaryIO
 from typing import Optional
 from typing import Union
 
-CHUNK_ENCODED_SIZE = 4  # number of base64 chars ber chunk
-CHUNK_DECODED_SIZE = 3  # numhber of raw bytes per chunk
+_E_SZ = 4  # (encoded size) number of base64 chars per chunk
+_D_SZ = 3  # (decoded size) number of raw bytes per chunk
 
 
 class Base64File(io.BufferedIOBase):
     """
-    The GzipFile class simulates most of the methods of a file object, with the exception of the truncate() method.
+    The GzipFile class simulates most of the methods of a file object.
 
     This class only supports opening files in binary mode.
     If you need to open a compressed file in text mode, use the gzip.open() function.
@@ -25,48 +25,58 @@ class Base64File(io.BufferedIOBase):
                  alt_chars: Optional[str] = None,
                  ):
         """
-        At least one of fileobj and file_name must be given a
+        At least one of file_obj and file_name must be given a
         non-trivial value.
 
-        The new class instance is based on fileobj, which can be a regular
+        The new class instance is based on file_obj, which can be a regular
         file, an io.BytesIO object, or any other object which simulates a file.
         It defaults to None, in which case file_name is opened to provide
         a file object.
 
-        When fileobj is not None, the file_name argument is only used to be
+        When file_obj is not None, the file_name argument is only used to be
         included in the gzip file header, which may include the original
         file_name of the uncompressed file.  It defaults to the file_name of
-        fileobj, if discernible; otherwise, it defaults to the empty string,
+        file_obj, if discernible; otherwise, it defaults to the empty string,
         and in this case the original file_name is not included in the header.
 
         The mode argument can be any of 'r', 'rb', 'a', 'ab', 'w', 'wb', 'x', or
         'xb' depending on whether the file will be read or written.  The default
-        is the mode of fileobj if discernible; otherwise, the default is 'rb'.
+        is the mode of file_obj if discernible; otherwise, the default is 'rb'.
         A mode of 'r' is equivalent to one of 'rb', and similarly for 'w' and
         'wb', 'a' and 'ab', and 'x' and 'xb'.
         """
 
         # we need at least one of these
         if file_obj is None and file_name is None:
-            raise ValueError('either file_name or fileobj must be specified')
+            raise ValueError('either file_name or file_obj must be specified')
 
-        # normalize mode to one of {'rb', 'wb', 'ab', 'xb'}
-        if mode is None:
-            mode = getattr(file_obj, 'mode', 'rb')
-        # if mode in {'r', 'rb', 'w', 'wb', 'a', 'ab', 'x', 'xb'}:  # todo
-        #     if 'b' not in mode:
-        #         mode += 'b'
-        #         warnings.warn(f'base64_file only supports binary, changing mode to {mode}')
-        # else:
-        #     raise ValueError(f'invalid mode "{mode}"')
-        self.mode = mode
+        # file object provided
+        if file_obj is not None:
+            self._opened_file_obj = None
+            if file_name is not None and file_name != getattr(file_obj, 'name', None):
+                warnings.warn(f'specified file_name "{file_name}" will be ignored, and file_obj will be used as-is')
 
-        # create fileobj if needed
-        self.my_file_obj = None
-        if file_obj is None:
-            file_obj = self.my_file_obj = builtins.open(file_name, mode or 'rb')
-        elif file_name is not None:
-            warnings.warn(f'specified file_name "{file_name}" will be ignored, and file_obj will be used as-is')
+            # what is the current mode
+            self.mode = getattr(file_obj, 'mode', mode)
+            if mode is not None and mode != self.mode:
+                warnings.warn(f'specified mode "{mode}" will be ignored, and file_obj will be used as-is')
+
+        # file object has to be created from file_name and mode
+        else:
+            if mode is None:
+                mode = 'rb'
+            if mode and set(mode).issubset({'r', 'w', 'a', 'x', '+', 'b'}):
+                if 'b' not in mode:
+                    mode += 'b'
+                    warnings.warn(f'base64_file only supports binary, changing mode to {mode}')
+            else:
+                raise ValueError(f'invalid mode "{mode}"')
+            self.mode = mode
+
+            # create file obj
+            file_obj = self._opened_file_obj = builtins.open(file_name, mode or 'rb')
+
+        # keep track of file object
         self.file_obj = file_obj
 
         # allow reading/writing to happen from the middle of a file
@@ -111,14 +121,10 @@ class Base64File(io.BufferedIOBase):
     def _ensure_readable(self):
         if not self.readable():
             raise io.UnsupportedOperation('File not open for reading')
-        # if self.mode is not 'rb':  # todo
-        #     raise IOError(f'specified mode is {self.mode}, file should not be readable')
 
     def _ensure_writable(self):
         if not self.writable():
             raise io.UnsupportedOperation('File not open for writing')
-        # if self.mode is 'rb':  # todo
-        #     raise IOError('specified mode is "rb", file should not be writable')
 
     def _ensure_seekable(self):
         if not self.seekable():
@@ -128,10 +134,10 @@ class Base64File(io.BufferedIOBase):
         self._ensure_not_closed()
         self._ensure_writable()
 
-        assert 0 <= self._buffer_cursor <= len(self._buffer) <= 3 and self._buffer_cursor < 3
+        assert 0 <= self._buffer_cursor <= len(self._buffer) <= _D_SZ and self._buffer_cursor < _D_SZ
 
         # check expected file position
-        _expected_tell = self.file_tell_offset + 4 * ((self._cursor - self._buffer_cursor) // 3)
+        _expected_tell = self.file_tell_offset + _E_SZ * ((self._cursor - self._buffer_cursor) // _D_SZ)
         if _expected_tell != self.file_obj.tell():  # maybe we read ahead a bit
             self._ensure_seekable()
             self.file_obj.seek(_expected_tell)
@@ -145,7 +151,7 @@ class Base64File(io.BufferedIOBase):
         self._cursor += len(data)
         _tmp.clear()
 
-        _num_writable_bytes = 3 * (self._buffer_cursor // 3)
+        _num_writable_bytes = _D_SZ * (self._buffer_cursor // _D_SZ)
         self.file_obj.write(base64.b64encode(self._buffer[:_num_writable_bytes], altchars=self.alt_chars))
 
         self._buffer, _tmp = self._buffer[_num_writable_bytes:], self._buffer
@@ -154,9 +160,9 @@ class Base64File(io.BufferedIOBase):
         self._data_not_written_flag = self._buffer_cursor != 0
 
         # sanity check
-        assert (self._cursor - self._buffer_cursor) % 3 == 0  # we've written a whole number of chunks
-        assert self.file_tell_offset + 4 * ((self._cursor - self._buffer_cursor) // 3) == self.file_obj.tell()
-        assert 0 <= self._buffer_cursor <= len(self._buffer) <= 3 and self._buffer_cursor < 3
+        assert (self._cursor - self._buffer_cursor) % _D_SZ == 0  # we've written a whole number of chunks
+        assert self.file_tell_offset + _E_SZ * ((self._cursor - self._buffer_cursor) // _D_SZ) == self.file_obj.tell()
+        assert 0 <= self._buffer_cursor <= len(self._buffer) <= _D_SZ and self._buffer_cursor < _D_SZ
 
         return len(data)
 
@@ -164,7 +170,7 @@ class Base64File(io.BufferedIOBase):
         self._ensure_not_closed()
         self._ensure_readable()  # cannot read if writing
 
-        assert 0 <= self._buffer_cursor <= len(self._buffer) <= 3 and self._buffer_cursor < 3
+        assert 0 <= self._buffer_cursor <= len(self._buffer) <= _D_SZ and self._buffer_cursor < _D_SZ
 
         # no-op
         if size == 0:
@@ -173,55 +179,55 @@ class Base64File(io.BufferedIOBase):
         # check expected file position
         if self._data_not_written_flag:
             self._ensure_seekable()
-            _expected_write_tell = self.file_tell_offset + 4 * ((self._cursor - self._buffer_cursor) // 3)
+            _expected_write_tell = self.file_tell_offset + _E_SZ * ((self._cursor - self._buffer_cursor) // _D_SZ)
             assert _expected_write_tell == self.file_obj.tell()
-            assert 0 <= self._buffer_cursor < 3
+            assert 0 <= self._buffer_cursor < _D_SZ
             self._ensure_seekable()
 
             # read one chunk of data, appending to existing unwritten bytes
-            if len(self._buffer) < 3:
+            if len(self._buffer) < _D_SZ:
                 _tmp, self._buffer = self._buffer, bytearray()
-                self._buffer.extend(base64.b64decode(self.file_obj.read(4), altchars=self.alt_chars, validate=True))
+                self._buffer.extend(base64.b64decode(self.file_obj.read(_E_SZ), altchars=self.alt_chars, validate=True))
                 self._buffer[:self._buffer_cursor] = _tmp[:self._buffer_cursor]
 
             # don't write anything (yet)
-            if len(self._buffer) < 3 or (size > 0 and self._buffer_cursor + size < 3):
+            if len(self._buffer) < _D_SZ or (size > 0 and self._buffer_cursor + size < _D_SZ):
                 self.file_obj.seek(_expected_write_tell)  # return file cursor to where we should write from
                 if size < 0:
                     size = len(self._buffer)
                 out = self._buffer[self._buffer_cursor:self._buffer_cursor + size]
                 self._buffer_cursor += len(out)
                 self._cursor += len(out)
-                assert 0 <= self._buffer_cursor <= len(self._buffer) <= 3 and self._buffer_cursor < 3
+                assert 0 <= self._buffer_cursor <= len(self._buffer) <= _D_SZ and self._buffer_cursor < _D_SZ
                 return bytes(out)
 
             # write one chunk of data
-            assert len(self._buffer) == 3
+            assert len(self._buffer) == _D_SZ
             self.file_obj.seek(_expected_write_tell)
-            self.file_obj.write(base64.b64encode(self._buffer[:3], altchars=self.alt_chars))
+            self.file_obj.write(base64.b64encode(self._buffer[:_D_SZ], altchars=self.alt_chars))
             self._data_not_written_flag = False
 
         # prepare to read data
-        assert len(self._buffer) in {0, 3}
-        assert 0 <= self._buffer_cursor <= len(self._buffer) <= 3 and self._buffer_cursor < 3
-        assert (self._cursor - self._buffer_cursor) % 3 == 0
-        _expected_tell = self.file_tell_offset + 4 * (self._cursor // 3)
+        assert len(self._buffer) in {0, _D_SZ}
+        assert 0 <= self._buffer_cursor <= len(self._buffer) <= _D_SZ and self._buffer_cursor < _D_SZ
+        assert (self._cursor - self._buffer_cursor) % _D_SZ == 0
+        _expected_tell = self.file_tell_offset + _E_SZ * (self._cursor // _D_SZ)
         if len(self._buffer) > 0:
-            _expected_tell += 4
+            _expected_tell += _E_SZ
         assert _expected_tell == self.file_obj.tell(), (_expected_tell, self.file_obj.tell())
 
         # figure out exactly how much to read
         if size > 0:
             _size_to_read = size - (len(self._buffer) - self._buffer_cursor)
-            _file_size_to_read = 4 * (_size_to_read // 3)
-            if _size_to_read % 3 > 0:
-                _file_size_to_read += 4
+            _file_size_to_read = _E_SZ * (_size_to_read // _D_SZ)
+            if _size_to_read % _D_SZ > 0:
+                _file_size_to_read += _E_SZ
         else:
             _file_size_to_read = -1
 
         # read data
         _bytes_read = self.file_obj.read(_file_size_to_read)
-        assert len(_bytes_read) % 4 == 0, (len(_bytes_read), _bytes_read)
+        assert len(_bytes_read) % _E_SZ == 0, (len(_bytes_read), _bytes_read)
         # if _file_size_to_read < 0 or len(_bytes_read) < _file_size_to_read:
         #     _bytes_read += b'===='
         self._buffer.extend(base64.b64decode(_bytes_read, altchars=self.alt_chars, validate=True))
@@ -236,18 +242,18 @@ class Base64File(io.BufferedIOBase):
 
         # clear read buffer
         _tmp, self._buffer = self._buffer, bytearray()
-        _num_bytes_to_clear = 3 * (self._buffer_cursor // 3)
+        _num_bytes_to_clear = _D_SZ * (self._buffer_cursor // _D_SZ)
         self._buffer.extend(_tmp[_num_bytes_to_clear:])
         _tmp.clear()
         self._buffer_cursor -= _num_bytes_to_clear
 
-        assert 0 <= self._buffer_cursor <= len(self._buffer) <= 3 and self._buffer_cursor < 3
+        assert 0 <= self._buffer_cursor <= len(self._buffer) <= _D_SZ and self._buffer_cursor < _D_SZ
         return out
 
     def close(self):
         # ensure data is written
         if self._data_not_written_flag:
-            self.read(3)
+            self.read(_D_SZ)
 
         # remove the file obj immediately
         file_obj, self.file_obj = self.file_obj, None
@@ -259,14 +265,15 @@ class Base64File(io.BufferedIOBase):
             file_obj.write(base64.b64encode(self._buffer, altchars=self.alt_chars))
 
         # close my_file_obj if we opened it via file_name in __init__
-        if self.my_file_obj is not None:
-            self.my_file_obj.close()
-            self.my_file_obj = None
+        if self._opened_file_obj is not None:
+            self._opened_file_obj.close()
+            self._opened_file_obj = None
 
     def flush(self):
         self._ensure_not_closed()
         self.file_obj.flush()
 
+    # noinspection SpellCheckingInspection
     def fileno(self):
         return self.file_obj.fileno()
 
@@ -277,7 +284,7 @@ class Base64File(io.BufferedIOBase):
 
         # ensure data is written
         if self._data_not_written_flag:
-            self.read(3)
+            self.read(_D_SZ)
 
         # write to end of file
         if self._data_not_written_flag:
@@ -286,15 +293,15 @@ class Base64File(io.BufferedIOBase):
         if whence == io.SEEK_SET:
             if offset < 0:
                 raise IOError('unable to seek to negative file size')
-            self._cursor = 3 * (offset // 3)
-            _offset = 4 * (offset // 3)
+            self._cursor = _D_SZ * (offset // _D_SZ)
+            _offset = _E_SZ * (offset // _D_SZ)
 
             self.file_obj.seek(_offset + self.file_tell_offset)
             self._buffer.clear()
             self._buffer_cursor = 0
 
-            if offset % 3:
-                self.read(offset % 3)
+            if offset % _D_SZ:
+                self.read(offset % _D_SZ)
 
         elif whence == io.SEEK_CUR:
             if self._cursor + offset < 0:
@@ -305,8 +312,8 @@ class Base64File(io.BufferedIOBase):
             _tmp = self.file_obj.tell()
             self.file_obj.seek(0, io.SEEK_END)
             _len = (self.file_obj.tell() - self.file_tell_offset)
-            self.file_obj.seek(self.file_tell_offset + 4 * (_len // 4))
-            self._cursor = 3 * (_len // 4)
+            self.file_obj.seek(self.file_tell_offset + _E_SZ * (_len // _E_SZ))
+            self._cursor = _D_SZ * (_len // _E_SZ)
             self.read()
 
         return self._cursor
@@ -316,10 +323,10 @@ class Base64File(io.BufferedIOBase):
 
 
 if __name__ == '__main__':
-    with open('tmp.txt', 'w+b') as f:
+    with open('tmp.txt', 'rb+') as f:
         print(f.write(b'\1\2'))
         print(f.write(b'\3'))
-        bf = Base64File(file_obj=f, mode='w')
+        bf = Base64File(file_obj=f)
         bf.write(b'01234567890123456789')
         bf.seek(1)
         print(bf.read(1))
@@ -327,6 +334,6 @@ if __name__ == '__main__':
         bf.close()
     with open('tmp.txt', 'rb') as f:
         f.seek(3)
-        bf = Base64File(file_obj=f, mode='w')
+        bf = Base64File(file_obj=f)
         print(bf.read())
         bf.close()
